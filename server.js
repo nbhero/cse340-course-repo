9342c3e11c2ecd6262fa12f15117429244107617
Bruno Rafael <brunorafael72@outlook.com>
2026-07-04 21:11:02 -0300

    EJS Template Created

---
+++ server.js
@@ -0,0 +1,47 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+/**
+  * Configure Express middleware
+  */
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// ------------------ Pages ---------------------------
+// Home
+app.get('/', async (req, res) => {
+    const title = 'Home';
+    res.render('home', { title });
+});
+// Organizations
+app.get('/organizations', async (req, res) => {
+    const title = 'Organizations';
+    res.render('organizations', { title });
+});
+// Projects
+app.get('/projects', async (req, res) => {
+    const title = 'Projects';
+    res.render('projects', { title });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+});
